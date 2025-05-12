4ef095587d3045ea8d7053e7b42cfdab769803bc
Sara-git-218 <sarah74218@gmail.com>
2025-05-12 18:01:09 +0300

    fix in program

--- MyShopWebApi/Program.cs
+++ MyShopWebApi/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICatgoryService, CatgoryService>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 builder.Services.AddDbContext<_326059268_ShopApiContext>(options =>
